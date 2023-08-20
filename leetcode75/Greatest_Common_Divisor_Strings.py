8f6572bbf261a5293aa45c058c294e521e59f747
Thi Phan <thiphan94@gmail.com>
2023-08-20 09:45:09 +0200

    update docstring

--- leetcode75/Greatest_Common_Divisor_Strings.py
+++ leetcode75/Greatest_Common_Divisor_Strings.py
@@ -2,13 +2,15 @@ import unittest
 
 
 class Solution:
+    """Solution class"""
+
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        """Given two strings, return the largest string x such that x divides both str1 and str2"""
         if len(str1) < len(str2):
             str1, str2 = str2, str1
 
         if str1 == str2:
             return str1
-
         if str1[: len(str2)] == str2:
             return self.gcdOfStrings(str1[len(str2) :], str2)
 
@@ -16,7 +18,10 @@ class Solution:
 
 
 class TestGreatestCommonDivisor(unittest.TestCase):
+    """Test cases"""
+
     def test_greatest_common_divisor(self):
+        """Test"""
         solution = Solution()
         self.assertEqual(solution.gcdOfStrings("ABCABC", "ABC"), "ABC")
         self.assertEqual(solution.gcdOfStrings("ABABAB", "ABAB"), "AB")
--- leetcode75/Place_Flowers.py
+++ leetcode75/Place_Flowers.py
@@ -3,8 +3,11 @@ from typing import List
 
 
 class Solution:
+    """Solution class"""
+
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count = 0
+        """Takes in a number n, returns true if n new flowers can be planted and false otherwise"""
+
         flowerbed = [0] + flowerbed + [0]
         for i in range(1, len(flowerbed) - 1):
             if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
@@ -15,7 +18,10 @@ class Solution:
 
 
 class TestGreatestCandies(unittest.TestCase):
+    """Test cases"""
+
     def test_place_flowers(self):
+        """Test"""
         solution = Solution()
         self.assertEqual(solution.canPlaceFlowers([1, 0, 0, 0, 1], 1), True)
         self.assertEqual(solution.canPlaceFlowers([1, 0, 0, 0, 1], 2), False)
