0f135eb0e34638a36ba96f112ef4026926f4d52e
Thi Phan <thiphan94@gmail.com>
2023-09-11 23:23:39 +0200

    article views

---
+++ 30dayspandas/article_views.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    # selecting rows based on condition
+    rslt_df = views[(views["author_id"] == views["viewer_id"] )]
+
+    return rslt_df[["author_id"]].rename(columns={"author_id": "id"}).drop_duplicates().sort_values(["id"], ascending=True)
