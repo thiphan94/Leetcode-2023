57e5d085bc9ca85b87504eec8b3151f102d9a203
Thi Phan <thiphan94@gmail.com>
2023-09-26 22:30:43 +0200

    majority element

---
+++ top150interview/majority_element.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = candidate = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
