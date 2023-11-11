767fca6282855b3d962c1e247443fff4e66f48de
Thi Phan <thiphan94@gmail.com>
2023-11-11 12:40:41 +0100

    factorial trailing zeroes

---
+++ top150interview/factorial_zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+
+        while n != 0:
+            count += n // 5
+            n = n // 5
+
+        return count
