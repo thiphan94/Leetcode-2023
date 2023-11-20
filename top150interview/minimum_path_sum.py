bcccbf03c466bbad752f9370221bee677445cccf
Thi Phan <thiphan94@gmail.com>
2023-11-20 23:46:23 +0100

    minimum path sum

---
+++ top150interview/minimum_path_sum.py
@@ -0,0 +1,16 @@
+from typing import List
+from itertools import product
+from math import inf
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        dp = [inf] * (n + 1)
+        dp[1] = 0
+        for i, j in product(range(m), range(n)):
+            k = j + 1
+            dp[k] = min(dp[k - 1], dp[k]) + grid[i][j]
+
+        return dp[-1]
