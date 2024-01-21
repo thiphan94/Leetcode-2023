a42b05b9aee87a1f8a9819a9a740c40e4a63d634
Thi Phan <thiphan94@gmail.com>
2024-01-21 19:43:32 +0100

    update

--- top150interview/word_break_dp.py
+++ top150interview/word_break_dp.py
@@ -1,12 +1,15 @@
+from typing import List
+
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         word_set = set(wordDict)
         dp = [False for i in range(len(s) + 1)]
         dp[len(s)] = True
-        for i in range(len(s)-1,-1,-1):
+        for i in range(len(s) - 1, -1, -1):
             for word in word_set:
-                if (i+len(word)) <= len(s) and s[i:i+len(word)] ==word:
-                    dp[i]=dp[i+len(word)]
+                if (i + len(word)) <= len(s) and s[i : i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
                 if dp[i]:
-                    break    
+                    break
         return dp[0]
