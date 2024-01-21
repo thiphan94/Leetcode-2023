a4c2eb1c0c5d318438c21226f717ec88408a78bb
Thi Phan <thiphan94@gmail.com>
2024-01-21 20:02:56 +0100

    sorted array to BST

---
+++ top150interview/sortedarray_BST.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        middle = len(nums) // 2
+
+        return TreeNode(
+            nums[middle],
+            self.sortedArrayToBST(nums[:middle]),
+            self.sortedArrayToBST(nums[middle + 1 :]),
+        )
