a87c6a653b07f3cba74b03e814b996bf1652e692
Thi Phan <thiphan94@gmail.com>
2024-02-19 05:54:55 +0700

    peak element

---
+++ top150interview/peak_element.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            middle = (left + right) // 2
+            if nums[middle] < nums[middle + 1]:
+                left = middle + 1
+            else:
+                right = middle
+        return left
