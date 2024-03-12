d16a938bf0d5c27200c167b15b51e128951c71ab
Thi Phan <thiphan94@gmail.com>
2024-03-12 21:41:43 +0100

    min stack

---
+++ top150interview/min_stack.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min:
+            self.min.append(min(val, self.min[-1]))
+        else:
+            self.min.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
